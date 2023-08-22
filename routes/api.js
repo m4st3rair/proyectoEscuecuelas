d1593ba3502081f645ed1f143a5418f03d1cdc73
m4st3rair <37892129+m4st3rair@users.noreply.github.com>
2023-08-21 20:03:52 -0600

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -18,22 +18,22 @@ router.get('/', async (req, res)=>{
 
 router.get('/webhook/', async (req, res)=>{
     console.log("get");
-    console.log(req.body);
+    console.log(req);
     res.send('OK');
 });
 router.put('/webhook/', async (req, res)=>{    
     console.log("put");
-    console.log(req.body);
+    console.log(req);
     res.send('OK');
 });
 router.post('/webhook/', async (req, res)=>{
     console.log("post");
-    console.log(req.body);
+    console.log(req);
     res.send('OK');
 });
 router.delete('/webhook/', async (req, res)=>{
     console.log("delete");
-    console.log(req.body);
+    console.log(req);
     res.send('OK');
 });
 
